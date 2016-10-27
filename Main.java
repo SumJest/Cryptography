fc30e2b84564cf755163abd678a4d11c7eec838a
TheSuJE <summedjesters@gmail.com>
2016-10-27 09:39:29 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,16 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -32,8 +23,23 @@ public class Main { // Main Class
 	private static File file;
 	private static String key = "qwerty123"; // Special key for EnCrypting
 	private static Dimension dim;
-	  
-	private static void createGUI(){
+
+	public static void main(String[] args) {
+		dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+		try {
+			System.out.println("Name: EnCrypting/Derypting Files\nversion: 0.5 Beta\nauthor: TheSuJe(Roman Fakhrutdinov)\ndate: 05 October 2016");
+
+			createGUI();
+
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void createGUI(){
   
 	final JFrame frame= new JFrame("Cryptography"); 
 	JPanel panel = new JPanel();
@@ -79,7 +85,7 @@ public class Main { // Main Class
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		frame.setSize(450, 450);
-		frame.setLocation(dim.width/2-450, dim.height/2-450);
+		frame.setLocation(dim.width/2, dim.height/2);
 		panel.setLayout(null);
 		JLabel lbl = new JLabel("Choose File to EnCrypt!");
 		lbl.setBounds(140, 5, 300, 15);
@@ -116,7 +122,7 @@ public class Main { // Main Class
 					if(choosera.getSelectedFile() != null){
 						try {
 							System.out.println(file1.getAbsolutePath());
-							encrypt(key, new FileInputStream(file), new FileOutputStream(choosera.getSelectedFile()));
+							CoreCrypt.encrypt(key, new FileInputStream(file), new FileOutputStream(choosera.getSelectedFile()));
 							System.out.println("a");
 						} catch (FileNotFoundException e1) {
 							e1.printStackTrace();
@@ -157,7 +163,7 @@ public class Main { // Main Class
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		frame.setSize(450, 450);
-		frame.setLocation(dim.width/2-450, dim.height/2-450);
+		frame.setLocation(dim.width/2, dim.height/2);
 		panel.setLayout(null);
 		JLabel lbl = new JLabel("Choose File to DeCrypt!");
 		lbl.setBounds(140, 5, 300, 15);
@@ -193,7 +199,7 @@ public class Main { // Main Class
 				choosera.showSaveDialog(txt);
 				if(choosera.getSelectedFile() != null){
 					try {
-						decrypt(key, new FileInputStream(file), new FileOutputStream(choosera.getSelectedFile()));
+						CoreCrypt.decrypt(key, new FileInputStream(file), new FileOutputStream(choosera.getSelectedFile()));
 					} catch (FileNotFoundException e1) {
 					
 						e1.printStackTrace();
@@ -227,53 +233,6 @@ public class Main { // Main Class
 		frame.setVisible(true);
 	}
 
-	public static void main(String[] args) {
-		dim = Toolkit.getDefaultToolkit().getScreenSize();
-		try {
-			System.out.println("Name: EnCrypting/Derypting Files\nversion: 0.5 Beta\nauthor: TheSuJe(Roman Fakhrutdinov)\ndate: 05 October 2016");
-
-			createGUI();
 
 
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void encrypt(String key, InputStream is, OutputStream os) throws Throwable {
-		encryptOrDecrypt(key, Cipher.ENCRYPT_MODE, is, os);
-	}
-
-	public static void decrypt(String key, InputStream is, OutputStream os) throws Throwable {
-		encryptOrDecrypt(key, Cipher.DECRYPT_MODE, is, os);
-	}
-
-	public static void encryptOrDecrypt(String key, int mode, InputStream is, OutputStream os) throws Throwable {
-
-		DESKeySpec dks = new DESKeySpec(key.getBytes());
-		SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
-		SecretKey desKey = skf.generateSecret(dks);
-		Cipher cipher = Cipher.getInstance("DES");  
-
-		if (mode == Cipher.ENCRYPT_MODE) {
-			cipher.init(Cipher.ENCRYPT_MODE, desKey);
-			CipherInputStream cis = new CipherInputStream(is, cipher);
-			doCopy(cis, os);
-		} else if (mode == Cipher.DECRYPT_MODE) {
-			cipher.init(Cipher.DECRYPT_MODE, desKey);
-			CipherOutputStream cos = new CipherOutputStream(os, cipher);
-			doCopy(is, cos);
-		}
-	}
-
-	public static void doCopy(InputStream is, OutputStream os) throws IOException {
-		byte[] bytes = new byte[64];
-		int numBytes;
-		while ((numBytes = is.read(bytes)) != -1) {
-			os.write(bytes, 0, numBytes);
-		}
-		os.flush();
-		os.close();
-		is.close();
-	}
 }
